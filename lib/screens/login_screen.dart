510299c0357daad69c8ea9d011fbb43f46954993
DeveloperKGH <developerkgh@gmail.com>
2022-09-14 20:53:18 +0900

    :sparkles: "login 화면 UI" - login 화면 UI 작업 중

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -116,7 +116,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 0, 20),
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -151,6 +151,92 @@ class _LoginScreenState extends State<LoginScreen> {
                 Divider(
                   color: const Color(0xff333333),
                   thickness: 1,
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(33, 13, 40, 13),
+                          child: Image.asset(
+                            'assets/images/kakaologo.png',
+                            width: 26,
+                            height: 24,
+                          ),
+                        ),
+                        Container(
+                          width: 160,
+                          height: 23,
+                          child: const FittedBox(
+                              fit: BoxFit.none,
+                              child: Text(
+                                '카카오로 로그인',
+                                overflow: TextOverflow.visible,
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: const Color(0xff101010),
+                                  letterSpacing: -0.32,
+                                ),
+                              )),
+                        )
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xffE8CF3F),
+                      minimumSize: Size.fromHeight(50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      padding: EdgeInsets.zero,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 12, 16, 0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(33, 13, 40, 13),
+                          child: Image.asset(
+                            'assets/images/glogo.png',
+                            width: 24,
+                            height: 24,
+                          ),
+                        ),
+                        Container(
+                          width: 160,
+                          height: 23,
+                          child: const FittedBox(
+                              fit: BoxFit.none,
+                              child: Text(
+                                '구글로 로그인',
+                                overflow: TextOverflow.visible,
+                                textAlign: TextAlign.left,
+                                style: TextStyle(
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.w500,
+                                  color: const Color(0xff101010),
+                                  letterSpacing: -0.32,
+                                ),
+                              )),
+                        )
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: const Color(0xffE2E2E2),
+                      minimumSize: Size.fromHeight(50),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(4.0)),
+                      padding: EdgeInsets.zero,
+                    ),
+                  ),
                 )
               ],
             ),
