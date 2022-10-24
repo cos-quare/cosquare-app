fcd569165653f418006f98d9f665c94febe63514
biso Yang <bisor0627@gmail.com>
2022-10-24 10:49:44 +0900

    :art: go_router 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,40 @@
-import 'package:costarica_app/screens/root_page.dart';
+import 'package:costarica_app/provider/authentication/authentication_provider.dart';
+import 'package:costarica_app/screens/auth/find_password_page.dart';
+import 'package:costarica_app/screens/auth/login_page.dart';
+import 'package:costarica_app/screens/auth/signup_page.dart';
 import 'package:costarica_app/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Costarica', theme: kCosTheme, home: RootPage());
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationProvider>(
+          create: (_) => AuthenticationProvider(),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: _router,
+        title: 'Costarica',
+        theme: kCosTheme,
+      ),
+    );
   }
+
+  final GoRouter _router = GoRouter(
+    routes: <GoRoute>[
+      GoRoute(path: '/', builder: (context, state) => LoginPage(), routes: <GoRoute>[
+        GoRoute(path: 'sign_up', builder: (context, state) => const SignUpPage()),
+        GoRoute(path: 'find_password', builder: (context, state) => const FindPasswordPage()),
+      ]),
+    ],
+    errorBuilder: (context, state) => LoginPage(),
+  );
 }
